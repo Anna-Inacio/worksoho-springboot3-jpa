d65db5cdeb1c72f5000246a8fc9dd6157fc4a845
Anna Inacio <anna.inacio@ifood.com.br>
2024-07-25 15:31:34 -0300

    User entity and resource

--- src/main/java/com/neliocurso/course/resources/UserResource.java
+++ src/main/java/com/neliocurso/course/resources/UserResource.java
@@ -1,4 +1,19 @@
 package com.neliocurso.course.resources;
 
+import com.neliocurso.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Anna", "anna@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
 }
